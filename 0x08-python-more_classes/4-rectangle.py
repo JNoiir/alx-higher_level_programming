dbcc2fda1e7fcc50617caa6233a14eddbc07e301
JNoiir <jenemali161@gmail.com>
2023-10-02 19:18:49 +0000

    rectangle rep

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -3,7 +3,7 @@
 
 
 class Rectangle:
-    """This gives the illustration of a rectangle"""
+    """This illustrates the representation of a rectangle"""
 
     def __init__(self, width=0, height=0):
         """Initializing this rectangle class
@@ -54,3 +54,18 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self) -> str:
+        """presents a diagram of the rectangle defined for an object"""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        rectangle = ""
+        for column in range(self.__height):
+            for row in range(self.__width):
+                rectangle += "#"
+            if column < self.__height - 1:
+                rectangle += "\n"
+        return (rectangle)
+
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
